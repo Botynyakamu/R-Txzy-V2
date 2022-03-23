886f8938109aaaab532ea3382f382688009b5853
Botynyakamu <102163283+Botynyakamu@users.noreply.github.com>
2022-03-23 11:49:27 +0700

    Update viagopay.js

--- plugins/viagopay.js
+++ plugins/viagopay.js
@@ -20,9 +20,6 @@ let info = `╠═〘 PEMBAYARAN 〙 ═
 ╠➥ *DAN 200 LEBIH*
 ║   *FITUR LAINNYA*
 ║ 
-╠═ ©2021 wabot-aq
-╠═ Scrip original by Nurutomo
-╠═〘 R-Txzy 〙 ═
 `.trim()
 
 conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '*Sewa Bot*', 'status@broadcast') 
