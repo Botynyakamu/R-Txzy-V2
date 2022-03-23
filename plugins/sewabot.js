fb14695305d832fc02f4a387c9925eeabda88c2d
Botynyakamu <102163283+Botynyakamu@users.noreply.github.com>
2022-03-23 11:47:43 +0700

    Update sewabot.js

--- plugins/sewabot.js
+++ plugins/sewabot.js
@@ -3,10 +3,9 @@ let { MessageType } = require('@adiwajshing/baileys')
 let handler = async(m, { conn }) => {
     let kamisato = `
 ┌〔 List Berlangganan 〕
-├ 2 Bulan
 ├ 1 Bulan
-├ 1 Minggu
-├ Trial 3 Hari
+├ 4 Bulan
+├ 8 Minggu
 └────
 Silahkan klik pada "List Harga" untuk melihat list.
 
@@ -18,11 +17,10 @@ GoPay: 085730903853
         buttonText: 'List Harga',
         description: kamisato,
         sections:  [{title: "Silahkan di pilih", rows: [
-        {title: '2 Bulan', description: "Rp15.000\nSewa bot tanpa batasan waktu.", rowId:".masuk"},
-        {title: '1 Bulan', description: "Rp10.000\nSewa bot selama 1 bulan.", rowId:".masuk"},
-        {title: '1 Minggu', description: "Rp7.000\nSewa bot selama 1 minggu.", rowId:".masuk"},
-        {title: 'Trial', description: "GRATIS\nBot gratis 3 Hari.", rowId:".join"},
-        {title: 'Owner', description: "Chat owner nya jika ada perlu.", rowId:".owner"},
+        {title: '1 Bulan', description: "Rp15.000\nSewa bot tanpa batasan waktu.", rowId:".masuk"},
+        {title: '4 Bulan', description: "Rp30.000\nSewa bot selama 1 bulan.", rowId:".masuk"},
+        {title: '8 Minggu', description: "Rp40.000\nSewa bot selama 1 minggu.", rowId:".masuk"},
+        {title: 'Owner', description: "Chat owner nya jika mau sewa.", rowId:".owner"},
         {title: 'Rules', description: "Kebijakan Privasi, Syarat Ketentuan dan Peraturan.", rowId:".snk"},
        ] }],
         listType: 1
